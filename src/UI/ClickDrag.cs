ee7d7cab902364d6e49854391924a17b74cf1e60
Tom Wilson <tomwilsoncoder@gmail.com>
2017-08-25 22:39:58 +0100

    Changed source headers
    Changed information about licensing for all source files.

--- src/UI/ClickDrag.cs
+++ src/UI/ClickDrag.cs
@@ -1,19 +1,12 @@
-/* 
- * This file is part of the RTS distribution (https://github.com/tomwilsoncoder/RTS)
- * 
- * This program is free software: you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
- * the Free Software Foundation, version 3.
+/*
+ *  THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ *  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+ *  PURPOSE. IT CAN BE DISTRIBUTED FREE OF CHARGE AS LONG AS THIS HEADER 
+ *  REMAINS UNCHANGED.
  *
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License 
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *  REPO: http://www.github.com/tomwilsoncoder/RTS
 */
-
 
 using System;
 using System.Drawing;
